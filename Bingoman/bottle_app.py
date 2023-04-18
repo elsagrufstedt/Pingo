2c4b9707d307ace1a709f9350dde50dfa78c7de1
fannybckman <fannybeckman01@gmail.com>
2023-04-18 15:17:35 +0200

    Fixade till den relativa sökvägen till templates

--- Bingoman/bottle_app.py
+++ Bingoman/bottle_app.py
@@ -1,4 +1,9 @@
-from bottle import run, route, template
+from bottle import run, route, template, TEMPLATE_PATH
+import os
+
+base_path = os.path.abspath(os.path.dirname(__file__))
+views_path = os.path.join(base_path, 'views')
+TEMPLATE_PATH.insert(0, views_path)
 
 @route("/")
 def index():
