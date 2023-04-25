c188f6244badb85add5568f97a53569eb40d8132
MaxHegdal <hegdalmax@gmail.com>
2023-04-25 21:11:31 +0200

    Start på databas

--- bottle_app.py
+++ bottle_app.py
@@ -1,88 +1,77 @@
 from bottle import run, route, template, TEMPLATE_PATH, static_file, request, redirect
 import os
 import json
+import sqlite3
+import hashlib
 
 base_path = os.path.abspath(os.path.dirname(__file__))
 views_path = os.path.join(base_path, 'views')
 TEMPLATE_PATH.insert(0, views_path)
 
 def read_categories():
-    try:
-        with open('views/static/categories.json', 'r',  encoding='utf-8') as f:
-            data = json.load(f)
-            return data
-    except FileNotFoundError:
-        with open('views/static/categories.json', 'w') as f:
-            f.write(json.dumps([]))
-            return []
-        
-def read_login():
-    try:
-        with open('views/static/login.json', 'r',  encoding='utf-8') as f:
-            data = json.load(f)
-            return data
-    except FileNotFoundError:
-        with open('views/static/login.json', 'w') as f:
-            f.write(json.dumps([]))
-            return []
-
-
-def create_id(user):
-    """
-    Returns an integer representing the current highest id + 1
-    
-    Returns
-        int : the current highest id + 1
-    """
-    highest_id = 1
-    for use in user:
-        if use["id"] >= highest_id:
-            highest_id = use["id"] + 1
-
-    return highest_id
-
+    with sqlite3.connect('pingo.db') as conn:
+        c = conn.cursor()
+        data = c.fetchall()
+        return data
 
 @route("/")
 def index():
     '''visar index.html'''
     return template("index")
 
+
 @route('/categories')
 def categories():
-    data = read_categories()
-    return template('views/categories', data=data)
+    with sqlite3.connect('pingo.db') as conn:
+        c = conn.cursor()
+
+        # Query the categories from the database
+        c.execute('''SELECT category, challenges FROM categories''')
+        data = c.fetchall()
+        categories = []
+    for row in data:
+        category = {
+            'category': row[0],
+            'challenges': json.loads(row[1])
+        }
+        categories.append(category)
+
+    return template('views/categories', data=categories)
 
 @route('/bingo/<category>')
 def bingo(category):
-    data = read_categories()
-    for item in data:
+    categories = read_categories()
+    challenges = []
+    for item in categories:
         if item['category'] == category:
-            return template('views/bingo', data=item['challenges'], category=category)
+            challenges = item['challenges']
+            break
+    return template('views/bingo', data=challenges, category=category)
 
-@route('/register', method=["GET"])
-def login():
+
+@route("/register", method=["GET"])
+def register():
     return template('views/register')
 
 @route("/register", method=["POST"])
 def create_user():
-    request.method =="POST"
+    request.method == "POST"
     email = getattr(request.forms, ("email"))
     password = getattr(request.forms, ("password"))
-    
-    new_user = read_login()
-    id = create_id(new_user)
-    new_user.append({
-        "email": email,
-        "password": password,
-        "id" : id
-    })
 
-    with open('views/static/login.json', 'w', encoding='utf-8') as f:
-        json.dump(new_user, f, indent=4)
+    # Generate a hash of the password using SHA-256
+    hash_obj = hashlib.sha256(password.encode())
+    password_hash = hash_obj.hexdigest()
+
+    # Insert the user data into the database
+    with sqlite3.connect('pingo.db') as conn:
+        c = conn.cursor()
+
+        c.execute('''INSERT INTO users (email, password) VALUES (?, ?)''',
+                  (email, password_hash))
 
     redirect("/")
         
-
 
 @route('/static/<filename:path>')
 def send_static(filename):
