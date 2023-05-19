f6925eb45d9a7026eef2c9db7791a8efd2034c77
elsagrufstedt <elsa.grufstedt@gmail.com>
2023-05-19 14:51:43 +0200

    Tog bort onödig kod

--- views/static/script.js
+++ views/static/script.js
@@ -91,9 +91,3 @@ function game_win() {
   return isGameWon;
 }
 
-function freeze(){
-    bingo_boxes.removeEventListener('click', box_check); 
-    if (game_win() === true){ 
-      Object.freeze(bingo_boxes);
-    }
-  }    
