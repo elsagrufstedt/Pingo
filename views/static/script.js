d511313010b1184fc9934b4d0d94fd5b18405230
peggydelvret <123165234+peggydelvret@users.noreply.github.com>
2023-05-30 15:47:33 +0200

    har en modal iaf?

--- views/static/script.js
+++ views/static/script.js
@@ -191,16 +191,28 @@ function showCharacterCount(input, characterCountId) {
   characterCountElement.textContent = remaining.toString();
 }
 
-//funktion för modal
-function showModal() {
-  var modal = document.getElementById("infoModal");
-  modal.classList.add("show");
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
   modal.style.display = "block";
 }
 
-function hideModal() {
-  var modal = document.getElementById("infoModal");
-  modal.classList.remove("show");
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
   modal.style.display = "none";
 }
 
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
